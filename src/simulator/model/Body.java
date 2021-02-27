2205446d50a656498da3eaa787e98331e8ca4f27
gulis1 <julian24082000@hotmail.com>
2021-02-28 00:01:06 +0100

    Se supone que hemos hecho body y massLosingBody

--- src/simulator/model/Body.java
+++ src/simulator/model/Body.java
@@ -1,4 +1,67 @@
 package simulator.model;
 
+import org.json.JSONObject;
+import simulator.misc.Vector2D;
+
 public class Body {
+
+    protected String id;
+    protected Vector2D vel, force, pos;
+    protected double mass;
+
+    public Body(String id, Vector2D vel, Vector2D pos, double mass) {
+        this.id = id;
+        this.vel = vel;
+        this.force = new Vector2D();
+        this.pos = pos;
+        this.mass = mass;
+    }
+
+    public void move(double t) {
+        Vector2D a;
+
+        if (mass == 0)
+            a = new Vector2D();
+
+        else
+            a = force.scale(1.0d/mass);
+
+        pos = pos.plus(vel.scale(t).plus(a.scale(0.5d*t*t)));
+        vel = vel.plus(a.scale(t));
+    }
+
+    public void resetForce() {
+        force = new Vector2D();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Vector2D getVel() {
+        return vel;
+    }
+
+    public Vector2D getForce() {
+        return force;
+    }
+
+    public Vector2D getPos() {
+        return pos;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public JSONObject getState() {
+        return new JSONObject().put("id", id).put("m", mass).put("p", pos.asJSONArray()).put("v",vel.asJSONArray()).put("f",force.asJSONArray());
+
+    }
+
+    /* { "id": id, "m": mass, "p": pos, "v": vel, "f": force }*/
+
+    public String toString() {
+        return getState().toString();
+    }
 }
---
+++ src/simulator/model/MassLosingBody.java
@@ -0,0 +1,30 @@
+package simulator.model;
+
+import simulator.misc.Vector2D;
+
+public class MassLosingBody extends Body{
+
+    protected double lossFactor, lossFrequency;
+    protected double c;
+
+    public MassLosingBody(String id, Vector2D vel, Vector2D pos, double mass, double lossFactor, double lossFrequency) {
+        super(id, vel, pos, mass);
+        this.lossFactor= lossFactor;
+        this.lossFrequency = lossFrequency;
+        this.c = 0.0d;
+
+    }
+
+    @Override
+    public void move(double t) {
+        super.move(t);
+
+        c += t;
+        if (c >= lossFrequency) {
+            mass = mass*(1.0d-lossFactor);
+            c = 0.0d;
+        }
+
+    }
+
+}
