b58ba2221e71d03f9c4957d8cf7f5643c27f4910
gulis1 <julian24082000@hotmail.com>
2021-04-26 17:28:07 +0200

    Panel de control casi terminado

--- src/simulator/view/ControlPanel.java
+++ src/simulator/view/ControlPanel.java
@@ -6,45 +6,155 @@ import simulator.model.SimulatorObserver;
 
 import javax.swing.*;
 import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
-public class ControlPanel extends JPanel implements SimulatorObserver {
+public class ControlPanel extends JToolBar implements SimulatorObserver {
     // ...
     private Controller _ctrl;
     private boolean _stopped;
     private String deltaTime = "2500.0";
 
+
     private int steps;
+
+    private JButton openButton;
+    private JButton physicsButton;
+    private JButton runButton;
+    private JButton stopButton;
+    private JButton exitButton;
+
+    private JSpinner fidgetSpinner;
+    private JTextField deltaVaina;
+
+    private JFrame padre;
 
     ControlPanel(Controller ctrl) {
         _ctrl = ctrl;
         _stopped = true;
         initGUI();
         _ctrl.addObserver(this);
-        this.setLayout(new FlowLayout(FlowLayout.LEFT));
+        this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 
 
     }
 
+    //se inicializan los botones
+    private void initButtons() {
+
+        openButton = new JButton(new ImageIcon("resources/icons/open.png"));
+        openButton.addActionListener(event -> {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setFileFilter(new FileNameExtensionFilter("Only json files", "json"));
+            int valor = fileChooser.showOpenDialog(this);
+
+            // Si se escoge un archivos
+            if (valor == JFileChooser.APPROVE_OPTION) {
+                _ctrl.reset();
+                try {
+                    _ctrl.loadBodies(new FileInputStream(fileChooser.getSelectedFile().getAbsoluteFile()));
+                }
+                catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+
+        physicsButton = new JButton(new ImageIcon("resources/icons/physics.png"));
+        physicsButton.addActionListener(event->{
+
+           JDialog setforces= new JDialog(padre,"Force Laws Selection",true);
+           //Font bold = new Font("Arial",Font.BOLD);
+            JTable table= new JTable();
+            JPanel panel = new JPanel();
+            JButton cancel=new JButton("cancel");
+            JButton ok=new JButton("OK");
+            JComboBox comboBox = new JComboBox();
+
+
+
+           setforces.setSize(800,500);
+           setforces.setLayout(new BorderLayout());
+           setforces.add(new JLabel("Select a force law  and provide values for the parameters in the Value column (default values are used for parameters with no value)"),BorderLayout.NORTH);
+           setforces.add(table,BorderLayout.CENTER);
+
+           panel.add(new JLabel("Force Law: "));
+           panel.add(comboBox);
+           panel.add(cancel);
+           panel.add(ok);
+
+           setforces.add(panel,BorderLayout.SOUTH);
+
+               // _ctrl.setForceLaws();
+            setforces.setVisible(true);
+
+        });
+
+
+        runButton = new JButton(new ImageIcon("resources/icons/run.png"));
+        runButton.addActionListener(event -> {
+
+            // Se desactivan todos los botones-
+            runButton.setEnabled(false);
+            openButton.setEnabled(false);
+            physicsButton.setEnabled(false);
+            exitButton.setEnabled(false);
+
+            _ctrl.setDeltaTime(Double.parseDouble(deltaTime));
+            run_sim(steps);
+
+        });
+
+        stopButton = new JButton(new ImageIcon("resources/icons/stop.png"));
+        stopButton.addActionListener(event -> _stopped = true);
+
+
+        exitButton = new JButton(new ImageIcon("resources/icons/exit.png"));
+        exitButton.addActionListener(event -> {
+            // Hemos hecho un poco el gamba aqui
+
+            int dialogResult = JOptionPane.showConfirmDialog(null, "Quieres salir?","Salir", JOptionPane.YES_NO_OPTION);
+
+            if (dialogResult == 0) {
+
+                dialogResult = JOptionPane.showConfirmDialog(null, "Seguro?","Pain-peko", JOptionPane.YES_NO_OPTION);
+
+                if (dialogResult == 0) {
+
+                    dialogResult = JOptionPane.showConfirmDialog(null, "De verdad?",":(", JOptionPane.YES_NO_OPTION);
+
+                    if (dialogResult == 0)
+                        System.exit(0);
+                }
+            }
+
+        });
+    }
+
+    private void initSpinnerAndTextField(){
+        fidgetSpinner = new JSpinner(new SpinnerNumberModel(steps, 0, null, 1000));
+        fidgetSpinner.setMaximumSize(new Dimension(600, 30));
+
+        deltaVaina = new JTextField(deltaTime);
+        deltaVaina.setMaximumSize(new Dimension(300 , 30));
+    }
+
     private void initGUI() {
-        JButton openButton = new JButton(new ImageIcon("resources/icons/open.png"));
-        JButton physicsButton = new JButton(new ImageIcon("resources/icons/physics.png"));
-        JButton runButton = new JButton(new ImageIcon("resources/icons/run.png"));
-        JButton stopButton = new JButton(new ImageIcon("resources/icons/stop.png"));
-        JButton exitButton = new JButton(new ImageIcon("resources/icons/exit.png"));
 
-        JSpinner fidgetSpinner = new JSpinner(new SpinnerNumberModel(steps, 0, null, 1000));
-        fidgetSpinner.setPreferredSize(new Dimension(100, 30));
-
-        JTextField deltaVaina = new JTextField(deltaTime);
-        deltaVaina.setPreferredSize(new Dimension(100 , 30));
+        initButtons();
+        initSpinnerAndTextField();
 
         this.add(openButton);
         this.add(physicsButton);
         this.add(runButton);
         this.add(stopButton);
-
 
         this.add(new JLabel("Steps:"));
 
@@ -52,7 +162,10 @@ public class ControlPanel extends JPanel implements SimulatorObserver {
         this.add(new JLabel("Delta-Time:"));
         this.add(deltaVaina);
 
+        this.add(Box.createGlue());
         this.add(exitButton);
+
+
 
 
 
@@ -64,38 +177,7 @@ public class ControlPanel extends JPanel implements SimulatorObserver {
     // other private/protected methods
 // ...
 
-    private class FidgetSpinnerModel implements SpinnerModel {
 
-        @Override
-        public Object getValue() {
-            return null;
-        }
-
-        @Override
-        public void setValue(Object value) {
-
-        }
-
-        @Override
-        public Object getNextValue() {
-            return null;
-        }
-
-        @Override
-        public Object getPreviousValue() {
-            return null;
-        }
-
-        @Override
-        public void addChangeListener(ChangeListener l) {
-
-        }
-
-        @Override
-        public void removeChangeListener(ChangeListener l) {
-
-        }
-    }
 
     private void run_sim(int n) {
         if (n > 0 && !_stopped) {
@@ -121,7 +203,10 @@ public class ControlPanel extends JPanel implements SimulatorObserver {
         else {
             _stopped = true;
 
-            // TODO enable all buttons
+            runButton.setEnabled(true);
+            openButton.setEnabled(true);
+            physicsButton.setEnabled(true);
+            exitButton.setEnabled(true);
         }
     }
 
